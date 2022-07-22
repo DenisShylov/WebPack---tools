211908caf1da1cceec54bb44bc2282e1b5e410bd
DenisShylov <dodjer123@gmail.com>
2022-07-22 17:12:42 +0300

    task3

---
+++ lesson6/task3/src/list/changeToCheckbox.js
@@ -0,0 +1,27 @@
+import { setItem, getItem } from './storage.js';
+import { renderTasks } from './renderTasks.js';
+
+export const changeToCheckbox = (event) => {
+  const isCheckbox = event.target.classList.contains('list__item-checkbox');
+
+  if (!isCheckbox) {
+    return;
+  }
+
+  const tasksList = getItem('tasksList');
+
+  const newTasksList = tasksList.map((task) => {
+    if (task.id === event.target.dataset.id) {
+      const done = event.target.checked;
+      return {
+        ...task,
+        done,
+        finishDate: done ? new Date().toISOString() : null,
+      };
+    }
+    return task;
+  });
+  setItem('tasksList', newTasksList);
+
+  renderTasks();
+};
